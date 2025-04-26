18cce5d07e79d59d975140a840d7541fcf0534e8
Kai Chen <chenkai0210@hotmail.com>
2025-04-26 20:33:42 +0800

    update script

--- pytorch-transformer/model.py
+++ pytorch-transformer/model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import math 
+import math
 
 """
 https://www.youtube.com/watch?v=ISNdQcPhsts&t=5s
@@ -37,21 +37,24 @@ class PositionalEncoding(nn.Module):
         # Create a matrix of size (seq_len, d_model)
         pe = torch.zeros(seq_len, d_model)
         # Create a vector of size (seq_len, 1)
-        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1) 
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model))
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)
+        )
         # Apply the sine to the even indices in the array
         pe[:, 0::2] = torch.sin(position * div_term)
-        # Apply the cosine to the odd indices in the array 
-        pe[:, 1::2] = torch.cos(position * div_term) 
+        # Apply the cosine to the odd indices in the array
+        pe[:, 1::2] = torch.cos(position * div_term)
 
         # Add a batch dimension
-        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
-        self.register_buffer('pe', pe) # Register the buffer so that it is not a parameter
-    
+        pe = pe.unsqueeze(0)  # (1, seq_len, d_model)
+        self.register_buffer(
+            "pe", pe
+        )  # Register the buffer so that it is not a parameter
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # Add the positional encoding to the input tensor
-        x = x + (self.pe[:, :x.shape(1), :]).requires_grad_(False)
+        x = x + (self.pe[:, : x.shape(1), :]).requires_grad_(False)
         # Apply dropout
         return self.dropout(x)
 
@@ -61,12 +64,12 @@ class PositionalEncoding(nn.Module):
 
 class LayerNormalization(nn.Module):
 
-    def __init__(self, eps:float=10**-6) -> None:
+    def __init__(self, eps: float = 10**-6) -> None:
         super().__init__()
         self.eps = eps
-        self.alpha = nn.Parameter(torch.ones(1)) # Multiplied
-        self.bias = nn.Parameter(torch.zeros(1)) # Added
-    
+        self.alpha = nn.Parameter(torch.ones(1))  # Multiplied
+        self.bias = nn.Parameter(torch.zeros(1))  # Added
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         mean = x.mean(-1, keepdim=True)
         std = x.std(-1, keepdim=True)
@@ -77,14 +80,14 @@ class FeedforwardBlock(nn.Module):
 
     def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
         super().__init__()
-        self.linear_1 = nn.Linear(d_model, d_ff) # W1 and B1
-        self.dropout = nn.Dropout(dropout) 
-        self.linear_2 = nn.Linear(d_ff, d_model) # W2 and B2
-    
+        self.linear_1 = nn.Linear(d_model, d_ff)  # W1 and B1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)  # W2 and B2
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # (Batch, Seq_len, d_model) -> (Batch, Seq_len, d_ff) -> (Batch, Seq_len, d_model)
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
-    
+
 
 class MultiHeadAttentionBlock(nn.Module):
 
@@ -94,21 +97,49 @@ class MultiHeadAttentionBlock(nn.Module):
         self.h = h
         assert self.d_model % self.h == 0, "d_model must be divisible by h"
         self.d_k = d_model // h
-        self.w_q = nn.Linear(d_model, d_model) # Wq
-        self.w_k = nn.Linear(d_model, d_model) # Wk
-        self.w_v = nn.Linear(d_model, d_model) # Wv
-        self.w_o = nn.Linear(d_model, d_model) # Wo
+        self.w_q = nn.Linear(d_model, d_model)  # Wq
+        self.w_k = nn.Linear(d_model, d_model)  # Wk
+        self.w_v = nn.Linear(d_model, d_model)  # Wv
+        self.w_o = nn.Linear(d_model, d_model)  # Wo
         self.dropout = nn.Dropout(dropout)
-    
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout) -> torch.Tensor:
+        d_k = query.shape[-1]  # d_k
+
+        # Calculate the attention scores
+        # (Batch, h, Seq_len, d_k) @ (Batch, h, d_k, Seq_len) -> (Batch, h, Seq_len, Seq_len)
+        # The @ operator is equivalent to torch.matmul
+        # The transpose method is equivalent to torch.transpose
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores = attention_scores.masked_fill(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(
+            dim=-1
+        )  # (Batch, h, Seq_len, Seq_len)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        return (
+            attention_scores @ value
+        ), attention_scores  # (Batch, h, Seq_len, d_k) @ (Batch, h, Seq_len, d_k) -> (Batch, h, Seq_len, d_model)
+
     def forward(self, q, k, v, mask) -> torch.Tensor:
-        query = self.w_q(q) # (Batch, Seq_len, d_model) -> (Batch, Seq_len, d_model)
-        key = self.w_k(k) # (Batch, Seq_len, d_model) -> (Batch, Seq_len, d_model)
-        value = self.w_v(v) # (Batch, Seq_len, d_model) -> (Batch, Seq_len, d_model)
+        query = self.w_q(q)  # (Batch, Seq_len, d_model) -> (Batch, Seq_len, d_model)
+        key = self.w_k(k)  # (Batch, Seq_len, d_model) -> (Batch, Seq_len, d_model)
+        value = self.w_v(v)  # (Batch, Seq_len, d_model) -> (Batch, Seq_len, d_model)
         # Split the embedding into h heads
         # (Bach, Seq_len, d_model) -> (Batch, Seq_len, h, d_k) -> (Batch, h, Seq_len, d_k)
-        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2) # (Batch, h, Seq_len, d_k)
-        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2) # (Batch, h, Seq_len, d_k)
-        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2) # (Batch, h, Seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(
+            1, 2
+        )  # (Batch, h, Seq_len, d_k)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(
+            1, 2
+        )  # (Batch, h, Seq_len, d_k)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(
+            1, 2
+        )  # (Batch, h, Seq_len, d_k)
 
-
-    
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(
+            query, key, value, mask, self.dropout
+        )
