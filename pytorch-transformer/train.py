c2a564897b15fe4e46d4639aae28875893a22772
Kai Chen <chenkai0210@hotmail.com>
2025-05-04 09:52:25 +0800

    update script

--- pytorch-transformer/train.py
+++ pytorch-transformer/train.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn
+from torch.utils.data import DataLoader, Dataset, random_split
 
 from datasets import load_dataset
 from tokenizers import Tokenizer
@@ -10,11 +11,16 @@ from tokenizers.pre_tokenizers import Whitespace
 from pathlib import Path
 
 
+def get_all_sentences(ds, lang):
+    for item in ds:
+        yield item["translation"][lang]
+
+
 def get_or_build_tokenizer(config, ds, lang):
     # config['tokenizer'] = '../tokenizer/tokenizer_{0}.json'
     tokenizer_path = Path(config["tokenizer_dir"].format(lang))
     if not Path.exists(tokenizer_path):
-        print("Tokenizer not found, building tokenizer")
+        # print("Tokenizer not found, building tokenizer")
         tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
         tokenizer.pre_tokenizer = Whitespace()
         trainer = WordLevelTrainer(
@@ -22,5 +28,26 @@ def get_or_build_tokenizer(config, ds, lang):
             min_frequency=2,
             special_tokens=["[UNK]", "[PAD]", "[CLS]", "[SEP]", "[MASK]"],
         )
-        tokenizer.train_from_iterator(ds["train"]["text"], trainer=trainer)
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
         tokenizer.save(tokenizer_path)
+    else:
+        # print("Tokenizer found, loading tokenizer")
+        tokenizer = Tokenizer.from_file(tokenizer_path)
+
+
+def get_ds(config):
+    ds_raw = load_dataset(
+        "opus_books", f'{config["lang_src"]}-{config["lang_tgt"]}', split="train"
+    )
+
+    # build tokenizer
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config["lang_src"])
+    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config["lang_tgt"])
+
+    # keep 90% of the dataset for training and 10% for validation
+    train_ds_size = int(len(ds_raw) * 0.9)
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
+    
+    ds = ds_raw.train_test_split(test_size=0.1)
+    ds = ds["train"].shuffle(seed=42)
