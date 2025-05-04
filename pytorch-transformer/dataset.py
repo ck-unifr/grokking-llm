aaf835898cdc6fdc1d3ef7d68aa263a120d405fc
Kai Chen <chenkai0210@hotmail.com>
2025-05-04 17:51:24 +0800

    update script

--- pytorch-transformer/dataset.py
+++ pytorch-transformer/dataset.py
@@ -17,21 +17,45 @@ class BilingualDataset(Dataset):
         self.tgt_lang = tgt_lang
         self.seq_len = seq_len
 
-        self.sos_token = torch.Tensor([tokenizer_src.token_to_id("[SOS]")], dtype=torch.int64)
-        self.eos_token = torch.Tensor([tokenizer_src.token_to_id("[EOS]")], dtype=torch.int64)
-        self.pad_token = torch.Tensor([tokenizer_src.token_to_id("[PAD]")], dtype=torch.int64)
-        self.unk_token = torch.Tensor([tokenizer_src.token_to_id("[UNK]")], dtype=torch.int64)
-    
+        self.sos_token = torch.Tensor(
+            [tokenizer_src.token_to_id("[SOS]")], dtype=torch.int64
+        )
+        self.eos_token = torch.Tensor(
+            [tokenizer_src.token_to_id("[EOS]")], dtype=torch.int64
+        )
+        self.pad_token = torch.Tensor(
+            [tokenizer_src.token_to_id("[PAD]")], dtype=torch.int64
+        )
+        self.unk_token = torch.Tensor(
+            [tokenizer_src.token_to_id("[UNK]")], dtype=torch.int64
+        )
+
     def __len__(self):
         return len(self.ds)
-    
+
     def __getitem__(self, index):
         src_target_pair = self.ds[index]
-        src_text = src_target_pair['translation'][self.src_lang]
-        tgt_text = src_target_pair['translation'][self.tgt_lang]
-        enc_input_tokens= self.tokenizer_src.encode(src_text).ids
+        src_text = src_target_pair["translation"][self.src_lang]
+        tgt_text = src_target_pair["translation"][self.tgt_lang]
+
+        enc_input_tokens = self.tokenizer_src.encode(src_text).ids
         dec_input_tokens = self.tokenizer_tgt.encode(tgt_text).ids
-        enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2 
+
+        enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2
         dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) - 1
 
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
+            raise ValueError("The input sequence is too long for the model.")
 
+        # add sos and eos tokens
+        encoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.Tensor(enc_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.Tensor(
+                    [self.pad_token] * enc_num_padding_tokens, dtype=torch.int64
+                ),
+            ]
+        )
+        
